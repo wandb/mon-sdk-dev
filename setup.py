5336d9382324433db6d176f056ce8f5cb2ae783b
Shawn Lewis <shlewis@gmail.com>
2023-03-30 07:44:49 -0400

    Fix setup again

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 from setuptools import setup
 import pkg_resources
 
+
+def convert_git_requirement(req):
+    # If requirements contains a git repo like
+    # git+https://github.com/wandb/client.git@dtypes/safer_image_restore#egg=wandb
+    # convert it to "wandb @ git+https://github.com/wandb/client.git@dtypes/safer_image_restore#egg=wandb""
+    if "egg=" in req:
+        _, package_name = req.split("egg=", 1)
+        return f"{package_name} @ {req}"
+    return req
+
+
 with open("requirements.txt") as requirements_file:
-    requirements = pkg_resources.parse_requirements(requirements_file) 
+    requirements = [
+        convert_git_requirement(str(requirement))
+        for requirement in pkg_resources.parse_requirements(requirements_file)
+    ]
 
 setup(
-    name="mon-sdk-deve",
+    name="mon-sdk-dev",
     version="0.0.1.dev1",
     description="W&B dev package for monitoring sdk.",
     author="Weights & Biases",
